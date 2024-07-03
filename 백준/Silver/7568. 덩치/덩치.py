f0f9862f2b7c9b41f1576a5c045be8da7f4af01d
yu-teacher <119153725+yu-teacher@users.noreply.github.com>
2024-07-03 20:40:31 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -3,13 +3,15 @@ people = []
 
 for _ in range(N):
     x, y = map(int, input().split())
-    people.append((x, y, 1))
+    people.append((x, y))
 
+ranks = []
 for i in range(N):
+    rank = 1
     for j in range(N):
         if i != j:
             if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
-                people[i] = (people[i][0], people[i][1], people[i][2] + 1)
+                rank += 1
+    ranks.append(rank)
 
-ranks = [str(person[2]) for person in people]
-print(" ".join(ranks))
+print(" ".join(map(str, ranks)))
