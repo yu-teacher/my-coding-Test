99aad7baed148a6fb40bdd483ec04809524f8415
yu-teacher <119153725+yu-teacher@users.noreply.github.com>
2024-07-03 15:35:05 +0900

    [Silver IV] Title: 대칭 차집합, Time: 264 ms, Memory: 88612 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+print(len(A.symmetric_difference(B)))
