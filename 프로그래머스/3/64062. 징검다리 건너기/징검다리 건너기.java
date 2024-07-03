c3699acdbf7484634440c244b83e45ad5419d5a3
yu-teacher <119153725+yu-teacher@users.noreply.github.com>
2024-07-03 22:29:59 +0900

    [level 3] Title: 징검다리 건너기, Time: 15.05 ms, Memory: 61.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,28 @@
+class Solution {
+   public int solution(int[] stones, int k) {
+        int min = 1;
+        int max = 200000001;
+        int result = 0;
+        while(min <= max) {
+            int mid = (min + max) / 2;
+            if(check(mid, k, stones)) {
+                min = mid + 1;
+                result = mid;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return result;
+    }
+    
+    public boolean check(int mid, int k, int[] stones) {
+        int count = 0;
+        for(int i = 0; i < stones.length; i++) {
+            if(stones[i] < mid) {
+                count++;
+                if(count >= k) return false;
+            } else count = 0;
+        }
+        return true;
+    }
+}
